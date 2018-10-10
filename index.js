d73548486f6c8c685a665e0f7727a61eb3067421
Kathleen Hung <a01044903@se14l0120-11.ad.bcit.ca>
2018-10-10 13:39:35 -0700

    added index.js

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ function RandomRed(){
 	var red = Math.round(Math.random()*255)
 	return "rgb("+red+", 100, 100)";
 
-}
-
-setInterval(()=>{
-	app.style.backgroundColor="rgb (255,222,255)";
-},500);
-
+	}
+	var i = 0;
+	setInterval(()=>{
+	if (i%2==0){
+	 app.style.backgroundColor= RandomRed();
+	} else {
+	   app.style.backgroundColor = RandomBlue();
+	}
+	i++;
+	}, 500);
 //B
 function RandomBlue(){
 var blue = Math.round(Math.random()*255)
